c4f6ae946c61f979cb3310b13147920728379594
ibrahim shittu <ibshittu01@gmail.com>
2022-04-10 17:41:01 +0100

    TS types - literal types

--- Understanding TS/app.js
+++ Understanding TS/app.js
@@ -1,19 +1,19 @@
 function combine(n1, n2, resultConversion) {
     var result;
-    if (typeof n1 === 'number' && typeof n2 === 'number') {
-        result = n1 + n2;
+    if (typeof n1 === 'number' && typeof n2 === 'number' || resultConversion === 'number') {
+        result = +n1 + +n2;
     }
     else {
         result = n1.toString() + n2.toString();
     }
-    if (resultConversion === 'as-number') {
-        return +result;
-    }
-    else {
-        return result.toString();
-    }
+    return result;
+    // if (resultConversion === 'as-number') {
+    //     return +result
+    // } else {
+    //     return result.toString()
+    // }
 }
-var combinedAges = combine(30, 20, 'string');
-var combinedAgesString = combine("30", "20", 'string');
+var combinedAges = combine(30, 20, 'number');
+var combinedAgesString = combine("30", "20", 'number');
 var combinesNames = combine("Ibrahim ", "Shittu", 'string');
 console.log(combinedAges, combinesNames, combinedAgesString);
