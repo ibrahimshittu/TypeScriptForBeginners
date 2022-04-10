0bf833651ea574d958119d99c9c08fe6b08bf12e
ibrahim shittu <ibshittu01@gmail.com>
2022-04-10 18:04:18 +0100

    function type w/ return type

--- Understanding TS/app.js
+++ Understanding TS/app.js
@@ -4,7 +4,15 @@ var add = function (a, b) {
 function printResult(num) {
     console.log('Result: ' + num);
 }
+function AddandHandle(a, b, cb) {
+    var result = a + b;
+    cb(result);
+}
 printResult(add(5, 12));
 var combinedValues; // Function type
+var combinedValues2; // Function type with return type
 combinedValues = add;
 console.log(combinedValues(8, 8));
+AddandHandle(10, 20, function (result) {
+    console.log(result);
+});
--- Understanding TS/app.ts
+++ Understanding TS/app.ts
@@ -6,10 +6,21 @@ function printResult(num: number): void { // Function with void return type
     console.log('Result: ' + num);
 }
 
+function AddandHandle(a: number, b: number, cb: (num: number) => void) { // Function with callback
+    const result = a + b;
+    cb(result);
+}
+
 printResult(add(5, 12));
 
 let combinedValues: Function; // Function type
+let combinedValues2: (a: number, b: number) => number; // Function type with return type
 
 combinedValues = add;
 
 console.log(combinedValues(8, 8));
+
+AddandHandle(10, 20, (result) => {
+    console.log(result);
+}
+);
