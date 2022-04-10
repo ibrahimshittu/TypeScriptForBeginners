4fe47cbb6c6fa835604dcb5692d82da1c71c6a06
ibrahim shittu <ibshittu01@gmail.com>
2022-04-10 17:35:51 +0100

    TS type - Union

--- Understanding TS/app.js
+++ Understanding TS/app.js
@@ -1,11 +1,19 @@
-function combine(n1, n2) {
+function combine(n1, n2, resultConversion) {
+    var result;
     if (typeof n1 === 'number' && typeof n2 === 'number') {
-        return n1 + n2;
+        result = n1 + n2;
     }
     else {
-        return n1.toString() + n2.toString();
+        result = n1.toString() + n2.toString();
+    }
+    if (resultConversion === 'as-number') {
+        return +result;
+    }
+    else {
+        return result.toString();
     }
 }
-var combinedAges = combine(30, 20);
-var combinesNames = combine("Ibrahim ", "Shittu");
-console.log(combinedAges, combinesNames);
+var combinedAges = combine(30, 20, 'string');
+var combinedAgesString = combine("30", "20", 'string');
+var combinesNames = combine("Ibrahim ", "Shittu", 'string');
+console.log(combinedAges, combinesNames, combinedAgesString);
--- Understanding TS/app.ts
+++ Understanding TS/app.ts
@@ -1,11 +1,18 @@
-function combine(n1: string | number, n2: string | number) { // Union type
-
+function combine(n1: string | number, n2: string | number, resultConversion: string) { // Union type
+    let result;
     if (typeof n1 === 'number' && typeof n2 === 'number') {
-        return n1 + n2;
+        result = n1 + n2;
     } else {
-        return n1.toString() + n2.toString();
+        result = n1.toString() + n2.toString();
+    }
+
+    if (resultConversion === 'as-number') {
+        return +result
+    } else {
+        return result.toString()
     }
 }
-const combinedAges = combine(30, 20)
-const combinesNames = combine("Ibrahim ", "Shittu")
-console.log(combinedAges, combinesNames);
+const combinedAges = combine(30, 20, 'string')
+const combinedAgesString = combine("30", "20", 'string')
+const combinesNames = combine("Ibrahim ", "Shittu", 'string');
+console.log(combinedAges, combinesNames, combinedAgesString);
